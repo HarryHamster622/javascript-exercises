a43a8c70f6828844d943f7ff8c4c6adb2df91e28
HarryHamster622 <56613758+HarryHamster622@users.noreply.github.com>
2024-01-29 17:53:25 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, final) {
+    var sum = 0;
+    if (first > final) {
+        var temp = first;
+        first = final;
+        final = temp;
+    } 
+    if (first < 0 || final < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(first) || !Number.isInteger(final)) {
+        return "ERROR";
+    }
+
+    for (first; first <= final; first++) {
+        sum = sum + first;
+       }
+return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
