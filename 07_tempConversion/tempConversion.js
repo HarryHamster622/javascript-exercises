94a3676b446f7a279637769f067ae75e5d23c6c7
HarryHamster622 <56613758+HarryHamster622@users.noreply.github.com>
2024-01-29 18:10:42 -0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fahrenheit = (temp - 32) * 5 / 9;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let celsius = temp * 9 / 5 + 32;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
 // Do not edit below this line
